dafda01fcb361f23029a4b1ee39363c96c218ed2
letrunghungprovip123 <138652865+letrunghungprovip123@users.noreply.github.com>
2026-09-21 11:21:56 +0700

    Add frontend service: frontend-service/src/main.jsx

---
+++ frontend-service/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import 'antd/dist/reset.css';
+import App from './App';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          borderRadius: 10,
+          colorPrimary: '#1677ff'
+        }
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
